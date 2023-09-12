8187a4d16731aa7299833e13beeff85b6ff8d95c
Doson <karbushev-1991@mail.ru>
2023-09-12 16:54:49 +0700

    Сохранение каждой страницы категории, добавил функция обнаружения блокировки, включил сравнение с яндексом

--- Sber.py
+++ Sber.py
@@ -1,3 +1,5 @@
+import time
+
 from driver import init_webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
@@ -28,7 +30,7 @@ class Card:
 
     def __str__(self):
         return f'{self.name} {self.price} {self.bonus_percentage} ' \
-               # f'{self.yandex_price}'
+            f'{self.yandex_price}'
 
 
 def close_promo(driver: Chrome):
@@ -46,16 +48,16 @@ def close_promo(driver: Chrome):
 
 
 @benchmark
-def get_cards_category(driver_sber: Chrome, url: str, thanks_percentage: int):
+def get_cards_category(driver_sber: Chrome, driver_ya, url: str, thanks_percentage: int):
     driver_sber.get(url)
-    driver_sber.implicitly_wait(1)
     cards = WebDriverWait(driver_sber, 30).until(EC.presence_of_all_elements_located(
         (By.XPATH, '//*[@class="catalog-listing__items '
                    'catalog-listing__items_divider"]/div[@id]')))
+    driver_sber.implicitly_wait(1)
     data = []
     for card in cards:
-        money = WebDriverWait(card, 0.5).until(EC.presence_of_element_located(
-            (By.CLASS_NAME, 'item-money'))).text
+        money = WebDriverWait(card, 1).until(EC.presence_of_element_located(
+            (By.XPATH, './/*[@class="item-money"]'))).text
         money = money.split('\n')
         try:
             price = money[0]
@@ -65,15 +67,15 @@ def get_cards_category(driver_sber: Chrome, url: str, thanks_percentage: int):
             price_discount = money[2]
         except IndexError:
             continue
-        name = WebDriverWait(card, 0.5).until(EC.presence_of_element_located(
+        name = WebDriverWait(card, 1).until(EC.presence_of_element_located(
             (By.XPATH, './/*[@class="inner"]/div[@class="item-title"]'))).text
-        link = WebDriverWait(card, 0.5).until(EC.presence_of_element_located(
+        link = WebDriverWait(card, 1).until(EC.presence_of_element_located(
             (By.TAG_NAME, 'a'))).get_attribute('href')
-
-        # yandex_price, yandex_url = get_min_price(driver_ya, name)
+        # Работа яднекс маркета
+        yandex_price, yandex_url = get_min_price(driver_ya, name)
 
         card = Card(name, link, price, discount_percentage, price_discount,
-                    # yandex_price, yandex_url
+                    yandex_price, yandex_url
                     )
         # card = Card(name.text, link, money)
         print(card)
@@ -99,11 +101,12 @@ def main(url, thanks_percentage, headless=True):
     driver_sber.get(url)
     driver_sber.set_window_size(1920, 1080)
     # driver.implicitly_wait(1)
+    detect_blocked(driver_sber)
 
-    # url_ya = 'https://market.yandex.ru/'
-    # driver_ya = init_webdriver(headless)
-    # driver_ya.get(url_ya)
-    # driver_ya.set_window_size(1920, 1080)
+    url_ya = 'https://market.yandex.ru/'
+    driver_ya = init_webdriver(headless)
+    driver_ya.get(url_ya)
+    driver_ya.set_window_size(1920, 1080)
 
     try:
         close_promo(driver_sber)
@@ -113,40 +116,55 @@ def main(url, thanks_percentage, headless=True):
     cycle = 0
     data = []
     while True:
-        try:
-            # cards = get_cards_category(driver_sber, driver_ya, url, thanks_percentage)
+        # try:
             cards = get_cards_category(driver_sber=driver_sber,
+                                       driver_ya=driver_ya,
                                        url=url,
                                        thanks_percentage=thanks_percentage)
-            data.extend(cards)
-        except:
-            driver_sber.refresh()
-            # print(f'[-] Error parse page {cycle}')
-            cards = []
-        driver_sber.save_screenshot('get_cards_category.png')
-        try:
-            next_url = get_next_url(driver_sber)
-        except TimeoutException:
+            # cards = get_cards_category(driver_sber=driver_sber,
+            #                            url=url,
+            #                            thanks_percentage=thanks_percentage)
+            # data.extend(cards)
+        # except:
+            # driver_sber.refresh()
+            # # print(f'[-] Error parse page {cycle}')
+            # cards = get_cards_category(driver_sber, driver_ya, url, thanks_percentage)
+            # driver_sber.save_screenshot('get_cards_category.png')
+            try:
+                next_url = get_next_url(driver_sber)
+            except TimeoutException:
+                print(f'Страница №{cycle}')
+                driver_sber.save_screenshot('get_next_url.png')
+                break
+
+            url = next_url + '#' + filter
+            print('Количество товаров:', len(cards))
+            cycle += 1
             print(f'Страница №{cycle}')
-            driver_sber.save_screenshot('get_next_url.png')
-            break
 
-        url = next_url + '#' + filter
-        print('Количество товаров:', len(cards))
-        cycle += 1
-        print(f'Страница №{cycle}')
+            data = pd.DataFrame(cards)
+            save_db(data,
+                    path='Sber.db',
+                    table_name='Sber',
+                    # print_column=['name', 'money']
+                    )
 
-    data = pd.DataFrame(data)
-    save_db(data,
-            path='Sber.db',
-            table_name='Sber',
-            # print_column=['name', 'money']
-            )
+
+def detect_blocked(driver_sber):
+    if driver_sber.title == 'Ой. Запросы с вашего устройства похожи на автоматически':
+        print(f"{'-' * 20}Обранаружен блокировщик!!!{'-' * 20}\n"
+              "[?]На сайте Введите код с картинки \n ")
+        while True:
+            time.sleep(1)
+            if driver_sber.title != 'Ой. Запросы с вашего устройства похожи на автоматически':
+                print("[+] Код с картинки успешно введен")
+                break
 
 
 if __name__ == '__main__':
-    url = str(input('Введите url адрес вместе с фильтрами: '))
-    thanks_percentage = int(input("Введите мин % СберСпасибо: "))
+    # url = str(input('Введите url адрес вместе с фильтрами: '))
+    # thanks_percentage = int(input("Введите мин % СберСпасибо: "))
     # url = 'https://megamarket.ru/catalog/smartfony-android/#?filters=%7B%221B3347144BD148AF9B0CE4AFF47710F7%22%3A%5B%221%22%5D%7D'
-    # thanks_percentage = 35
-    main(url, thanks_percentage, True)
+    url = "https://megamarket.ru/catalog/televizory/"
+    thanks_percentage = 35
+    main(url, thanks_percentage, False)
