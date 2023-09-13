385f537891bed8c040051ede744a13fa028ec482
Doson <karbushev-1991@mail.ru>
2023-09-14 01:40:52 +0700

    добавил путь для драйвера

--- driver.py
+++ driver.py
@@ -1,8 +1,10 @@
+import os
+
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 # !pip install webdriver-manager
 from webdriver_manager.chrome import ChromeDriverManager
-# from fake_useragent import UserAgent
+from fake_useragent import UserAgent
 # pip install undetected-chromedriver
 import undetected_chromedriver as uc
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -16,19 +18,19 @@ def init_webdriver(headless=True):
     # chrome_options.add_argument('--start-fullscreen')
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--disable-popup-blocking")
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--log-level=3")
-
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-crash-reporter")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-in-process-stack-traces")
-    chrome_options.add_argument("--disable-logging")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--log-level=3")
-    chrome_options.add_argument("--output=/dev/null")
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument("--disable-popup-blocking")
+    # chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument("--log-level=3")
+    #
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--disable-crash-reporter")
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-in-process-stack-traces")
+    # chrome_options.add_argument("--disable-logging")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--log-level=3")
+    # chrome_options.add_argument("--output=/dev/null")
 
     """
     Загрузка файлов в указаную директорию
@@ -45,17 +47,22 @@ def init_webdriver(headless=True):
 
     """
     Fake user-agent
+"""
 
-    ua = UserAgent()
-    ua_random = ua.random
-    chrome_options.add_argument(f"user-agent={ua_random}")
-    """
+    def install_version():
+        ChromeDriverManager(
+            latest_release_url='https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/113.0.5672.63/win64/chromedriver-win64.zip').install()
 
-    driver = uc.Chrome(driver_executable_path=get_chromedriver_fp(),
-                       # executable_path=r'C:\install\chromedriver.exe',
-                       # browser_executable_path=r'C:\install\chrome-win64\chrome.exe',
-                       # executable_path=ChromeDriverManager(
-                       #        latest_release_url='https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/116.0.5845.96/win64/chromedriver-win64.zip').install(),
-                       options=chrome_options,
-                       )
+    # ua = UserAgent()
+    # ua_random = ua.random
+    # chrome_options.add_argument(f"user-agent={ua_random}")
+
+    driver = uc.Chrome(
+        # driver_executable_path=get_chromedriver_fp(),
+        driver_executable_path=os.getcwd() + r'\chromedriver.exe',
+        # browser_executable_path=r'C:\install\chrome-win64\chrome.exe',
+        # executable_path=ChromeDriverManager().install(),
+        options=chrome_options,
+        use_subprocess=False,
+    )
     return driver
